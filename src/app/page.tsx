019a7f9c75105a1ece72016aecdc3a5b71b89803
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-12-03 18:35:38 -0500

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Modal2 } from "../Components/modals/Modal2";
 import TokenListDialog from "../Components/Modals/TokenListDialog";
 import "../styles/index.css";
 import "../styles/modal.css";
+import "../styles/dialog.css";
 import "../styles/App.module.css";
 import "../styles/modalNEW.css";
 
@@ -14,48 +15,44 @@ export function App() {
   const [openModal2, setOpenModal2] = useState(false);
   return (
     <>
-    <div className="App">
-      <button
-        className="open-modal-button"
-        onClick={() => {
-          setOpenModal1(true);
-        }}
-      >
-        Subscribe
-      </button>
-      {openModal1 && <Modal1 openModal1={openModal1} setOpenModal1={setOpenModal1} />}
-   </div>
-    <div className="ModalButton">
-      <button
-        className="tokenList-modal-button"
-        onClick={() => {
-          setOpenModal2(true);
-        }}
-      >
-        OpenTokenListModal
-      </button>
-      {openModal2 && <Modal2 openModal2={openModal2} setOpenModal2={setOpenModal2} />}
+      <div className="App">
+        <button
+          className="open-modal-button"
+          onClick={() => {
+            setOpenModal1(true);
+          }}
+        >
+          Subscribe
+        </button>
+        {openModal1 && <Modal1 openModal1={openModal1} setOpenModal1={setOpenModal1} />}
     </div>
+      <div className="ModalButton">
+        <button
+          className="tokenList-modal-button"
+          onClick={() => {
+            setOpenModal2(true);
+          }}
+        >
+          OpenTokenListModal
+        </button>
+        {openModal2 && <Modal2 openModal2={openModal2} setOpenModal2={setOpenModal2} />}
+      </div>
 
-    <div className="ModalButton">
-      <button
-        className="tokenList-modal-button2"
-        onClick={() => {
-          const dialog = document.querySelector("dialog")
-          dialog.show()
-        }}
-      >
-        OpenDialogueModal
-      </button>
-    </div>
+      <div className="ModalButton">
+        <button
+          className="tokenList-modal-button2"
+          onClick={() => {
+            const dialog = document.querySelector("dialog")
+            dialog.show()
+          }}
+        >
+          OpenDialogueModal
+        </button>
+      </div>
 
-<TokenListDialog />
+      <TokenListDialog  />
 
-    {/* <dialog>
-      This is a dialog
-    </dialog>
- */}
-  </>
+    </>
   );
 }
 
